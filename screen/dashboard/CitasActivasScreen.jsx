80d6b40090dfd3ebefcf33c55f6120e5e7d3d4f1
Diego <119649331+818diego@users.noreply.github.com>
2024-04-29 13:03:48 -0700

    Update 0.5.1

--- screen/dashboard/CitasActivasScreen.jsx
+++ screen/dashboard/CitasActivasScreen.jsx
@@ -5,6 +5,9 @@ import { styles } from '../../themes/theme';
 
 import img from '../../assets/images/persona.jpg';
 import img2 from '../../assets/images/persona2.jpg';
+import img3 from '../../assets/images/persona3.jpg';
+import img4 from '../../assets/images/persona4.jpg';
+import img5 from '../../assets/images/persona5.jpg';
 
 const CitasActivasScreen = () => {
     return (
@@ -12,14 +15,14 @@ const CitasActivasScreen = () => {
             <Text style={styles.titleScreen}>Citas Activas</Text>
             <Card img={img} title="María Rodríguez" content="12/04/2024 - 02:30PM" />
             <Card img={img2} title="Juan Pérez" content="13/04/2024 - 10:45AM" />
-            <Card img={img} title="Luisa Gómez" content="14/04/2024 - 03:15PM" />
-            <Card img={img2} title="Carlos Martínez" content="15/04/2024 - 11:20AM" />
-            <Card img={img} title="Ana López" content="16/04/2024 - 01:55PM" />
-            <Card img={img2} title="Pedro Sánchez" content="17/04/2024 - 09:00AM" />
-            <Card img={img} title="Laura García" content="18/04/2024 - 04:30PM" />
-            <Card img={img2} title="Elena Martínez" content="19/04/2024 - 12:00PM" />
-            <Card img={img} title="María Rodríguez López" content="23/04/2024 - 01:30PM" />
-            <Card img={img2} title="Juan Pérez Martínez" content="24/04/2024 - 10:45AM" />
+            <Card img={img3} title="Luisa Gómez" content="14/04/2024 - 03:15PM" />
+            <Card img={img4} title="Carlos Martínez" content="15/04/2024 - 11:20AM" />
+            <Card img={img5} title="Ana López" content="16/04/2024 - 01:55PM" />
+            <Card img={img} title="Pedro Sánchez" content="17/04/2024 - 09:00AM" />
+            <Card img={img2} title="Laura García" content="18/04/2024 - 04:30PM" />
+            <Card img={img3} title="Elena Martínez" content="19/04/2024 - 12:00PM" />
+            <Card img={img4} title="María Rodríguez López" content="23/04/2024 - 01:30PM" />
+            <Card img={img5} title="Juan Pérez Martínez" content="24/04/2024 - 10:45AM" />
         </ScrollView>
     );
 };
--- screen/dashboard/SolicitudCitasScreen.jsx
+++ screen/dashboard/SolicitudCitasScreen.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import CardCitas from '../../components/CardsTypes/CardCitas';
+import { View, Text, ScrollView } from 'react-native';
 
 //se importan las imagenes de las personas
 import img from '../../assets/images/persona.jpg';
 import img2 from '../../assets/images/persona2.jpg';
-import { View, Text, ScrollView } from 'react-native';
+import img3 from '../../assets/images/persona3.jpg';
+import img4 from '../../assets/images/persona4.jpg';
+import img5 from '../../assets/images/persona5.jpg';
+
+import CardCitas from '../../components/CardsTypes/CardCitas';
 import { styles } from '../../themes/theme';
 
 const SolicitudCitasScreen = () => {
@@ -16,13 +20,12 @@ const SolicitudCitasScreen = () => {
             <Text style={styles.titleScreen}>Solicitud de Citas</Text>
             <CardCitas img={img} title="María García López" content="15/04/2024 - 10:30AM" />
             <CardCitas img={img2} title="Juan Pérez Rodríguez" content="16/04/2024 - 02:15PM" />
-            <CardCitas img={img} title="Ana Martínez Sánchez" content="17/04/2024 - 09:45AM" />
-            <CardCitas img={img2} title="Pedro González Martínez" content="18/04/2024 - 11:00AM" />
-            <CardCitas img={img} title="Laura Fernández García" content="19/04/2024 - 03:30PM" />
-            <CardCitas img={img2} title="Carlos Rodríguez Pérez" content="20/04/2024 - 12:45PM" />
-            <CardCitas img={img} title="Elena Sánchez Martínez" content="21/04/2024 - 08:00AM" />
-            <CardCitas img={img2} title="Luis López García" content="22/04/2024 - 04:20PM" />
-
+            <CardCitas img={img3} title="Ana Martínez Sánchez" content="17/04/2024 - 09:45AM" />
+            <CardCitas img={img4} title="Pedro González Martínez" content="18/04/2024 - 11:00AM" />
+            <CardCitas img={img5} title="Laura Fernández García" content="19/04/2024 - 03:30PM" />
+            <CardCitas img={img} title="Carlos Rodríguez Pérez" content="20/04/2024 - 12:45PM" />
+            <CardCitas img={img2} title="Elena Sánchez Martínez" content="21/04/2024 - 08:00AM" />
+            <CardCitas img={img3} title="Luis López García" content="22/04/2024 - 04:20PM" />
         </ScrollView>
 
     );
