47ce93ead02b1534b78da96356fb98926696be88
orrrrli <castaneda.kevin@uabc.edu.mx>
2024-04-29 14:06:18 -0700

    se agrego el calendar

--- screen/dashboard/CalendarioScreen.jsx
+++ screen/dashboard/CalendarioScreen.jsx
@@ -7,9 +7,7 @@ const CalendarioScreen = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.titleScreen}>Calendario</Text>
-            <View style={styles.calendarContainer}>
-                <WeekCalendar/>
-            </View>
+            <WeekCalendar/>
         </View>
     );
 };
